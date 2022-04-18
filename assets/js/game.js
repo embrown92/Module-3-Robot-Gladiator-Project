cfa34e8392d6fca071c40a85f24e75d97eebc62d
edamame2000 <eda_awe92@yahoo.com.ph>
2022-04-18 17:10:26 -0500

    added enemyName arrays and for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -6,7 +12,7 @@ var playerMoney = 10;
 
 //You can also log multiple values at once like this console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -67,4 +73,10 @@ var fight = function () {
 };
 
 // run fight function to start game
-fight();
+// fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " indext");
+}
